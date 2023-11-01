3ddc61a158126884c55f21c68986f01690ff8ba7
kymaka <seafth345@gmail.com>
2023-11-02 01:34:38 +0700

    Some more changes in error handling. TODO: change number type identifier.

--- main.go
+++ main.go
@@ -33,8 +33,12 @@ func main() {
 
 		operator := fields[1]
 		if numType == arabic {
-			a := parseNumber(fields[0])
-			b := parseNumber(fields[2])
+			a, errA := parseNumber(fields[0])
+			b, errB := parseNumber(fields[2])
+			if errA != nil || errB != nil {
+				log.Fatal(err)
+			}
+
 			result, err := calculate(operator, a, b)
 			if err != nil {
 				log.Fatal(err)
@@ -42,21 +46,27 @@ func main() {
 
 			fmt.Println(result)
 		} else if numType == roman {
-			a := parseRomanNumber(fields[0])
-			b := parseRomanNumber(fields[2])
+			a, _ := parseRomanNumber(fields[0])
+			b, _ := parseRomanNumber(fields[2])
 			result, err := calculate(operator, a, b)
 			if err != nil {
 				log.Fatal(err)
 			}
+			if result <= 0 {
+				log.Fatal("roman number can't be zero or negative")
+			}
 
-			fmt.Println(result)
+			fmt.Println(integerToRoman(result))
 		}
 	}
 }
 
 func getNumbersType(a string, b string) (NumberType, error) {
-	first := parseRomanNumber(a)
-	second := parseRomanNumber(b)
+	first, errFirst := parseRomanNumber(a)
+	second, errSecond := parseRomanNumber(b)
+	if errFirst != nil || errSecond != nil {
+		return wrong, errors.New("illegal roman number is supplied")
+	}
 
 	if first == 0 && second == 0 {
 		return arabic, nil
@@ -78,50 +88,83 @@ func getInputAsTokens(reader *bufio.Reader) ([]string, error) {
 	fmt.Println("Please enter an expression to calculate.")
 	line, err := reader.ReadString('\n') // Read string until enter is pressed.
 	if err != nil {
-		log.Fatal(err)
+		return []string{""}, errors.New("can't read line from input")
 	}
 
 	fields := strings.Fields(line) // Get items separated by spaces.
 	if len(fields) != 3 {
 		return nil, errors.New(expressionError)
 	}
-	return fields, nil
 
+	return fields, nil
 }
 
-func parseNumber(field string) int {
-
+func parseNumber(field string) (int, error) {
 	num, err := strconv.Atoi(field)
 	if err != nil {
-		log.Fatal("Please enter valid number.", err)
+		return num, err
 	}
-	return num
+
+	return num, nil
 }
 
-func parseRomanNumber(num string) int {
+func parseRomanNumber(num string) (int, error) {
 	switch num {
 	case "I":
-		return 1
+		return 1, nil
 	case "II":
-		return 2
+		return 2, nil
 	case "III":
-		return 3
+		return 3, nil
 	case "IV":
-		return 4
+		return 4, nil
 	case "V":
-		return 5
+		return 5, nil
 	case "VI":
-		return 6
+		return 6, nil
 	case "VII":
-		return 7
+		return 7, nil
 	case "VIII":
-		return 8
+		return 8, nil
 	case "IX":
-		return 9
+		return 9, nil
 	case "X":
-		return 10
+		return 10, nil
+	default:
+		return 0, errors.New("no allowed roman number is found")
 	}
-	return 0
+}
+
+func integerToRoman(number int) string {
+	maxRomanNumber := 100
+	if number > maxRomanNumber {
+		return strconv.Itoa(number)
+	}
+
+	conversions := []struct {
+		value int
+		digit string
+	}{
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
+
+	var roman strings.Builder
+	for _, conversion := range conversions {
+		for number >= conversion.value {
+			roman.WriteString(conversion.digit)
+			number -= conversion.value
+		}
+	}
+
+	return roman.String()
 }
 
 func calculate(operator string, a int, b int) (int, error) {
@@ -136,7 +179,7 @@ func calculate(operator string, a int, b int) (int, error) {
 	case "/":
 		result = a / b
 	default:
-		return result, errors.New("can't calculate")
+		return result, errors.New("bad operator")
 	}
 	return result, nil
 }
